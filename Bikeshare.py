4154bcb8829e173062c579bc9512136892bcff28
basti-repo <sebastian1605@gmail.com>
2021-03-10 12:32:30 +0100

    implement load-data function

--- Bikeshare.py
+++ Bikeshare.py
@@ -41,6 +41,34 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
+    # load data file into a dataframe
+    df = pd.read_csv(CITY_DATA[city.lower()])
+
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.day_of_week
+
+
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month.lower())+1
+
+        # filter by month to create the new dataframe
+        df = df[df['month']==month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # index of the day list to get the correspondig int
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        day = days.index(day.lower())
+
+        # filter by day of week to create the new dataframe
+        df =  df[df['day_of_week']==day]
 
     return df
 
@@ -135,3 +163,21 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+
+# filename = 'chicago.csv'
+#
+# # load data file into a dataframe
+# df = pd.read_csv(filename)
+#
+# # convert the Start Time column to datetime
+# df['Start Time'] = pd.to_datetime(df['Start Time'])
+#
+# # extract hour from the Start Time column to create an hour column
+# df['hour'] = df['Start Time'].dt.hour
+#
+# # find the most common hour (from 0 to 23)
+# popular_hour = df['hour'].value_counts().idxmax()
+# # !!!!OR popular_hour = df['hour'].mode()[0]!!!!
+#
+# print('Most Frequent Start Hour:', popular_hour)
